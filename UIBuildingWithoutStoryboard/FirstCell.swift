3a025e2b590a0761b6f1232985bd07f4e3505b8f
ujhr <muappscontact@gmail.com>
2019-09-17 23:41:42 +0900

    リファクタリング

---
+++ UIBuildingWithoutStoryboard/FirstCell.swift
@@ -0,0 +1,24 @@
+//
+//  FirstCell.swift
+//  UIBuildingWithoutStoryboard
+//
+//  Created by ユーザー１ on 2019/02/25.
+//  Copyright © 2019 Masaya Ujihara. All rights reserved.
+//
+
+import UIKit
+
+class FirstCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
